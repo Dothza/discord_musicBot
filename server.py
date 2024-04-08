9a52eea2d4c910a87d49a87bc99387bd7815bfad
Alexey <fjutsuu@mail.ru>
2024-04-09 00:31:36 +0300

    TZ created; command class created

--- server.py
+++ server.py
@@ -1,12 +1,35 @@
 import asyncio
 import discord
-import datetime as dt
 from discord.ext import commands
 import logging
 
-logging.basicConfig(filename="temporary.log")
-# TODO: Настроить базовое логирование
-
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="$", intents=intents)
+
+
+class DiscordPlay(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="play_audio")
+    async def music(self, ctx, url):
+        await ctx.send("Команда получена")
+
+
+bot = commands.Bot(command_prefix='!#', intents=intents)
+
+TOKEN = "BOTTOKEN"
+
+
+async def main():
+    await bot.add_cog(DiscordPlay(bot))
+    await bot.start(TOKEN)
+
+
+asyncio.run(main())
