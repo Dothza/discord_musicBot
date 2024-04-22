b076363338961b26762da579ca755e4c11e61906
HalVa48 <nikit97886@gmail.com>
2024-04-23 00:00:04 +0300

    bd created

--- data/download.py
+++ data/download.py
@@ -4,23 +4,23 @@ from data.db_session import *
 PARAMS = {'extract_audio': True, 'format': 'bestaudio', '--skip-unavailable-fragments': True, "extract_flat": True}
 
 
-async def download(url):
+def download(url):
     downloader = dlp.YoutubeDL(PARAMS)
     data = downloader.extract_info(url, download=False)
     try:
         if data["_type"] == "playlist":
             for i in data["entries"]:
                 song = Song()
-                song.name = i["titile"]
-                song.link = download(i["url"])
-                song.playlist_link = url
-                song.playlist_name = data["title"]
+                song.name = str(i["title"])
+                song.link = str((downloader.extract_info(i['url'], download=False))["url"])
+                song.playlist_link = str(url)
+                song.playlist_name = str(data["title"])
                 db_sess = create_session()
                 db_sess.add(song)
                 db_sess.commit()
     except KeyError:
         song = Song()
-        song.name = data["titile"]
+        song.name = data["title"]
         song.link = data["url"]
         db_sess = create_session()
         db_sess.add(song)
