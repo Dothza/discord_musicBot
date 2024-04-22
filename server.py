174672149981af86bc03e9f069d7a7fe39121716
vzffelnzes <104564290+vzffelnzes@users.noreply.github.com>
2024-04-22 22:12:09 +0300

    add ochko

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from data.download import download
 from discord.ext import commands
 import logging
 from discord.utils import get
+import random
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -46,14 +47,68 @@ class DiscordPlay(commands.Cog):
         await ctx.channel.send("Воспроизведение остановлено.")
 
     @commands.command(name="kick")
-    @commands.has_permissions(administration=True)
+    # @commands.has_permissions(administration=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         await ctx.channel.purge(limit=1)
         await member.kick(reason=reason)
         await ctx.channel.send(f"{member} был кикнут с сервера.")
 
+    @commands.command(name="prikol")
+    async def music(self, ctx):
+        channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        queue = await download("https://youtu.be/dQw4w9WgXcQ?si=NcPkSTzU4pe7SDsv")
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect(reconnect=True, timeout=None)
+        voice.play(discord.FFmpegPCMAudio(queue, executable="data/ffmpeg.exe"))
+        await ctx.channel.send("You have been rickrolled:)")
 
-TOKEN = "BOTTOKEN"
+    @commands.command(name="ochko")
+    async def game(self, ctx, cards_col):
+        cards = {"черви_6": 6, "черви_7": 7, "черви_8": 8, "черви_9": 9, "черви_10": 10, "черви_валет": 1,
+                 "черви_дама": 2, "черви_kороль": 3, "черви_туз": 11,
+                 "крести_6": 6, "крести_7": 7, "крести_8": 8, "крести_9": 9, "крести_10": 10, "крести_валет": 1,
+                 "крести_дама": 2, "крести_kороль": 3, "крести_туз": 11,
+                 "буби_6": 6, "буби_7": 7, "буби_8": 8, "буби_9": 9, "буби_10": 10, "буби_валет": 1, "буби_дама": 2,
+                 "буби_kороль": 3, "буби_туз": 11,
+                 "пики_6": 6, "пики_7": 7, "пики_8": 8, "пики_9": 9, "пики_10": 10, "пики_валет": 1, "пики_дама": 2,
+                 "пики_kороль": 3, "пики_туз": 11}
+        players_cards = []
+        players_points = 0
+        bots_points = 0
+        bots_cards = []
+        cards_col = int(cards_col)
+        if 0 < int(cards_col) <= 5:
+            for i in range(cards_col):
+                card = random.choice(list(cards.keys()))
+                players_points += cards[card]
+                players_cards.append(card)
+            for i in range(random.randint(1, 4)):
+                card = random.choice(list(cards.keys()))
+                bots_points += cards[card]
+                bots_cards.append(card)
+            if players_points > 21:
+                await ctx.channel.send("Перебор")
+                await ctx.channel.send(f"Ваши карты: {players_cards}")
+            elif bots_points > 21 >= players_points:
+                await ctx.channel.send("Вы выиграли")
+                await ctx.channel.send(f"Ваши карты: {players_cards}")
+                await ctx.channel.send(f"Карты бота: {bots_cards}")
+            elif bots_points <= players_points < 21:
+                await ctx.channel.send("Вы выиграли")
+                await ctx.channel.send(f"Ваши карты: {players_cards}")
+                await ctx.channel.send(f"Карты бота: {bots_cards}")
+            elif 21 >= bots_points > players_points:
+                await ctx.channel.send("Вы проиграли")
+                await ctx.channel.send(f"Ваши карты: {players_cards}")
+                await ctx.channel.send(f"Карты бота: {bots_cards}")
+        else:
+            await ctx.channel.send("Неподходящее число")
+
+
+TOKEN = "BOT_TOKEN"
 
 bot = commands.Bot(command_prefix="$", intents=intents)
 
