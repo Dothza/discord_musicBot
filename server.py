f3cb3169fe8a57848070859299e0645bea3edf9c
Dothza <fjutsuu@mail.ru>
2024-04-28 23:07:06 +0300

    commit fixed;

--- server.py
+++ server.py
@@ -2,60 +2,121 @@
 
 
 import asyncio
+import logging
+import os
+import random
 import discord
 import discord.message
 from data.download import download
 from discord.ext import commands
-import logging
 from discord.utils import get
-import random
+from data import db_session
+from data.db_session import *
+from data.song import Song
+
+OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+           'options': '-vn -filter:a "volume=0.25"'}
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-intents = discord.Intents.all()
+try:
+    os.remove("db/base.db")
+except FileNotFoundError:
+    pass
+finally:
+    db_session.global_init("db/base.db")
 
 
 class DiscordPlay(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(name="info")
-    async def help(self, ctx):
-        await ctx.channel.send(
-            "Музыкальный бот Discord\nКоманда $play - включить песню\nКоманда $stop - остановить "
-            "воспроизведение.\nКоманда $game - начать игру в 21.")
+        self.current_song = None
 
     @commands.command(name="play")
     async def music(self, ctx, url):
         channel = ctx.message.author.voice.channel
         voice = get(self.bot.voice_clients, guild=ctx.guild)
-        queue = await download(url)
+        await download(url)
         if voice and voice.is_connected():
             await voice.move_to(channel)
         else:
             voice = await channel.connect(reconnect=True, timeout=None)
-        voice.play(discord.FFmpegPCMAudio(queue, executable="data/ffmpeg.exe"))
-        await ctx.channel.send(f"{queue['title']} - воспроизводится.")
+        await self.start_song()
+        voice.play(discord.FFmpegOpusAudio(self.current_song.link, executable="data/ffmpeg.exe", **OPTIONS))
+        await ctx.channel.send(f"{self.current_song.name} - воспроизводится.")
+
+    async def start_song(self):
+        db_session = create_session()
+        if self.current_song:
+            self.current_song = db_session.query(Song).filter(Song.id == self.current_song.id + 1)[0]
+        else:
+            self.current_song = db_session.query(Song).filter(Song.id == 1)[0]
+
+    @commands.command(name="skip")
+    async def skip_song(self, ctx):
+        db_session = create_session()
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        try:
+            if self.current_song:
+                self.current_song = db_session.query(Song).filter(Song.id == self.current_song.id + 1)[0]
+            else:
+                self.current_song = db_session.query(Song).filter(Song.id == 1)[0]
+        except IndexError:
+            await ctx.channel.send("Очередь пуста. Начинаю сначала.")
+            self.current_song = db_session.query(Song).filter(Song.id == 1)[0]
+        if voice.is_playing:
+            voice.stop()
+            voice.play(discord.FFmpegOpusAudio(self.current_song.link, executable="data/ffmpeg.exe", **OPTIONS))
+            await ctx.channel.send(f"{self.current_song.name} - воспроизводится.")
+
+    @commands.command(name="info")
+    async def help(self, ctx):
+        await ctx.channel.send(
+            "Музыкальный бот Discord\nКоманда $play - включить песню\nКоманда $stop - остановить воспроизведение.")
 
     @commands.command(name="stop")
     async def stop_music(self, ctx):
+        db_session = create_session()
         voice = get(self.bot.voice_clients, guild=ctx.guild)
-        voice.stop()
-        await ctx.channel.send("Воспроизведение остановлено.")
+        if voice:
+            voice.stop()
+            await ctx.channel.send("Воспроизведение остановлено.")
+        else:
+            await ctx.channel.send("Ничего не проигрывается.")
+        db_session.query(Song).delete()
+        db_session.commit()
+
+    @commands.command(name="pause")
+    async def pause_music(self, ctx):
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice:
+            voice.pause()
+            await ctx.channel.send("Воспроизведение приостановлено.")
+        else:
+            await ctx.channel.send("Ничего не проигрывается.")
+
+    @commands.command(name="resume")
+    async def resume_music(self, ctx):
+        voice = get(self.bot.voice_clients, guild=ctx.guild)
+        if voice and voice.is_paused():
+            voice.resume()
+            await ctx.channel.send("Воспроизведение возобновлено.")
+        else:
+            await ctx.channel.send("Ничего не проигрывается.")
 
     @commands.command(name="kick")
-    # @commands.has_permissions(administration=True)
+    @commands.has_permissions(administrator=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         await ctx.channel.purge(limit=1)
         await member.kick(reason=reason)
         await ctx.channel.send(f"{member} был кикнут с сервера.")
 
-    @commands.command(name="prikol")
-    async def music(self, ctx):
+    @commands.command(name="joke")
+    async def rickroll(self, ctx):
         channel = ctx.message.author.voice.channel
         voice = get(self.bot.voice_clients, guild=ctx.guild)
         queue = await download("https://youtu.be/dQw4w9WgXcQ?si=NcPkSTzU4pe7SDsv")
@@ -63,10 +124,10 @@ class DiscordPlay(commands.Cog):
             await voice.move_to(channel)
         else:
             voice = await channel.connect(reconnect=True, timeout=None)
-        voice.play(discord.FFmpegPCMAudio(queue, executable="data/ffmpeg.exe"))
-        await ctx.channel.send("You have been rickrolled:)")
+        voice.play(discord.FFmpegOpusAudio(queue, executable="data/ffmpeg.exe", **OPTIONS))
+        await ctx.channel.send("You have been rickrolled :)")
 
-    @commands.command(name="ochko")
+    @commands.command(name="game")
     async def game(self, ctx, cards_col):
         cards = {"черви_6": 6, "черви_7": 7, "черви_8": 8, "черви_9": 9, "черви_10": 10, "черви_валет": 1,
                  "черви_дама": 2, "черви_kороль": 3, "черви_туз": 11,
@@ -109,9 +170,10 @@ class DiscordPlay(commands.Cog):
             await ctx.channel.send("Неподходящее число")
 
 
-TOKEN = "BOT_TOKEN"
+TOKEN = "TOKEN"
+intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 async def main():
